66b1e3c1faa628ff80a6a4d60f3e86c139b83fbe
Unknown <nedelec.loick@gmail.com>
2017-02-10 14:51:39 +0100

    debut de la création des tests sur les users

--- Hibernate/PizzaDomi/src/dao/Test_List_User.java
+++ Hibernate/PizzaDomi/src/dao/Test_List_User.java
@@ -1,5 +1,28 @@
 package dao;
 
-public class Test_List_User {
+import java.util.List;
 
+import bean.Pizza;
+import bean.User;
+
+public class Test_List_User {
+	  public static void main(String [] args) { 
+		     
+		    DAO dao = new DAO(); 
+		    dao.ouvrir(); 
+		     
+		    List<User> usrs = dao.listerUser(); 
+		     
+		    System.out.println(); 
+		    System.out.println("nb Users = "+usrs.size()); 
+		     
+		    for (User usr : usrs) { 
+		      System.out.println("User nom = "+usr.getId());
+		      for (Pizza pizz : usr.getFavoris()) {
+		    	  System.out.println("- Pizza : "+ pizz.getNom());
+		      }
+		    } 
+		     
+		    dao.fermer(); 
+		  } 
 }
